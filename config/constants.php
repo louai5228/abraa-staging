27d63eba4b4b4238ac42e115d91a42504e31aad9
louai alkhdr <eng.loe.khder@ramaaz.com>
2023-01-02 18:14:11 +0200

    add whatsapp services

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
     'whatsapp'  =>  [
-        'token' =>  '2uim1xj3v3u4pu1k',
-        'instance'  =>  'instance27478'
+        'token' =>  '2uim1xj3v3u1k',
+        'instance'  =>  'instance274'
     ]
 ];
